aa7940d4c4d956e994b3ad3aaa295b296358f6f2
Dalton Herrewynen <neuronmaker@gmail.com>
2021-09-24 02:29:11 -0600

    Update rotate-background.bash

--- rotate-background.bash
+++ rotate-background.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+# this script's job is to switch my desktop backgrounds (or maybe rotate if I wanted to and added the logic)
+# There must be two image files for night and day arbitrarily named, I originally planned to use it to switch to a black background at night
+# but I instead found it more useful to have it triggered by a keypress in my desktop environment so I can switch between two backgrounds with a single keystroke.
 CURRENTPAPER=$(gsettings get org.cinnamon.desktop.background picture-uri)
 
 NIGHTPAPER="file:///home/dalton/Pictures/Wallpapers/Using/night.jpg"
