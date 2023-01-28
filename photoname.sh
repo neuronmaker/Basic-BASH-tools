02d087fd1689cc24d70264aaa432be4db4fb9626
Dalton Herrewynen <neuronmaker@gmail.com>
2023-01-28 04:29:36 -0700

    Fixed a typo

--- photoname.sh
+++ photoname.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Exactly what the name says. It will feed the files you want to exiftool which is set to rename the photos to the time stamp which the camera put in the metadata
-# Reason I do this is my camera by default uses names like DSC_XXX.JPG or DSC_XXX.NEF where XXXX is a 4 digit numerical serial number, it will wrap around by default or reset to 0000 if a setting is set
-# Running this on all new photos ensures that there is no chance that two files will be overwritten for as long as I am onyl using one camera... But I plan to change this as I find better ways to organize my photos.
+# Exactly what the name says. It will feed the files you want into exiftool which is set to rename the photos to the time stamp which the camera put in the file's metadata
+# The reason I do this is my camera by default uses names like DSC_XXXX.JPG or DSC_XXXX.NEF where XXXX is a 4 digit numerical serial number, it will wrap around or reset depending on a certain camera setting... hence the reason this script exists.
+# Running this on all new photos ensures that there is no chance that files will be overwritten for as long as I am only using one camera at a time... But I plan to change this as I find better ways to organize my photos.
 printUsage () {
   echo "Usage: "
   echo "    $0 [item]"
@@ -14,7 +14,7 @@ FILE=$1
 if [ "$1" = "-h" ]; then
     printUsage
 fi
-
+#if no files passed, select everything in this directory, this is for lazy usage and can try to rename files in all subdirectories. Be careful with that.
 if [ -z "$1" ]; then
     FILE="*"
 fi
